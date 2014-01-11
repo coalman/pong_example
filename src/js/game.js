0e183f945fef231226b37d00a30c9c9c9049d8f1
Coalman <tehcoalman@gmail.com>
2014-01-11 01:19:31 -0500

    Added input for the second paddle and using the state manager now.

--- src/js/game.js
+++ src/js/game.js
@@ -1,41 +1,57 @@
-window.onload = function() {
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
-        preload: preload, 
-        create: create,
-        update: update
-    });
-    var paddle0;
-    var ball;
-    var cursors;
+var States = {};
 
-    function preload() {
-        game.load.image('paddle0', 'img/paddleBlu.png');
-        game.load.image('paddle1', 'img/paddleRed.png');
-        game.load.image('ball0', 'img/ballGrey.png');
-    }
+States.Game = function(game) {
+    this.game = game;
+    this.paddle0 = null;
+    this.paddle1 = null;
+    this.ball = null;
+};
+States.Game.prototype = {
+    preload: function() {
+        this.game.load.image('paddle0', 'img/paddleBlu.png');
+        this.game.load.image('paddle1', 'img/paddleRed.png');
+        this.game.load.image('ball0', 'img/ballGrey.png');
+    },
 
-    function create() {
-        paddle0 = game.add.sprite(game.world.centerX, game.world.height - 100, 'paddle0');
-        paddle0.anchor.setTo(0.5, 0.5);
+    create: function() {
+        var add = this.game.add;
 
-        paddle1 = game.add.sprite(game.world.centerX, 100, 'paddle1');
-        paddle1.anchor.setTo(0.5, 0.5);
+        this.paddle0 = add.sprite(this.game.world.centerX,
+            this.game.world.height - 50, 'paddle0');
+        this.paddle0.anchor.setTo(0.5, 0.5);
 
-        ball = game.add.sprite(game.world.centerX, game.world.centerY, 'ball0');
-        ball.anchor.setTo(0.5, 0.5);
+        this.paddle1 = add.sprite(this.game.world.centerX,
+            50, 'paddle1');
+        this.paddle1.anchor.setTo(0.5, 0.5);
 
-        cursors = game.input.keyboard.createCursorKeys();
-    }
+        this.ball = add.sprite(this.game.world.centerX,
+            this.game.world.centerY, 'ball0');
+        this.ball.anchor.setTo(0.5, 0.5);
+    },
 
-    function update() {
+    update: function() {
         var input = 0;
-        if (cursors.right.isDown) {
+        if (this.game.input.keyboard.isDown(Phaser.Keyboard.A)) {
+            input -= 250;
+        } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.S)) {
             input += 250;
         }
-        if (cursors.left.isDown) {
+
+        this.paddle0.body.velocity.x = input;
+        input = 0;
+
+        if (this.game.input.keyboard.isDown(Phaser.Keyboard.K)) {
             input -= 250;
+        } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.L)) {
+            input += 250;
         }
 
-        paddle0.body.velocity.x = input;
+        this.paddle1.body.velocity.x = input;
     }
+};
+
+window.onload = function() {
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+    game.state.add('Game', States.Game);
+    game.state.start('Game');
 };
