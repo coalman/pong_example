ee6dc807efc3d8237089855ea303cbd7597c6794
Coalman <tehcoalman@gmail.com>
2014-01-12 21:27:13 -0500

    Added basic ball movement/collision.

--- src/js/game.js
+++ src/js/game.js
@@ -2,9 +2,11 @@ var States = {};
 
 States.Game = function(game) {
     this.game = game;
+    this.paddles = null;
     this.paddle0 = null;
     this.paddle1 = null;
     this.ball = null;
+    this.ballAlive = false;
 };
 States.Game.prototype = {
     preload: function() {
@@ -15,14 +17,17 @@ States.Game.prototype = {
 
     create: function() {
         var add = this.game.add;
+        this.paddles = add.group();
 
         this.paddle0 = add.sprite(this.game.world.centerX,
             this.game.world.height - 50, 'paddle0');
         this.paddle0.anchor.setTo(0.5, 0.5);
+        this.paddles.add(this.paddle0);
 
         this.paddle1 = add.sprite(this.game.world.centerX,
             50, 'paddle1');
         this.paddle1.anchor.setTo(0.5, 0.5);
+        this.paddles.add(this.paddle1);
 
         this.ball = add.sprite(this.game.world.centerX,
             this.game.world.centerY, 'ball0');
@@ -47,6 +52,22 @@ States.Game.prototype = {
         }
 
         this.paddle1.body.velocity.x = input;
+
+        if (!this.ballAlive && 
+            this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+            this.ballAlive = true;
+
+            this.ball.body.velocity.y = 250;
+        }
+
+        if (this.ballAlive) {
+            this.game.physics.collide(this.ball, this.paddles,
+                this.ballCollision, null, this);
+        }
+    },
+
+    ballCollision: function() {
+        this.ball.body.velocity.y *= -1;
     }
 };
 
